f6a63c84e4e6ba06a7b743831bef01720c7ca087
Mooffie <mooffie@gmail.com>
2015-12-19 23:37:01 +0200

    175. module prompts: getting user input

--- src/lua/modules/prompts.c
+++ src/lua/modules/prompts.c
@@ -32,7 +32,7 @@
 #include <config.h>
 
 #include "lib/global.h"
-#include "lib/widget.h"         /* message() */
+#include "lib/widget.h"         /* input_dialog(), message() */
 #include "lib/lua/capi.h"
 #include "lib/lua/plumbing.h"   /* mc_lua_ui_is_ready() */
 
@@ -43,10 +43,13 @@
 
 /*** file scope macro definitions ****************************************************************/
 
+#define MC_HISTORY_LUA_DEFAULT "mc.lua.default"
+
 /*** file scope type declarations ****************************************************************/
 
 /*** forward declarations (file scope functions) *************************************************/
 
+static int l_input (lua_State * L);
 static int l_alert (lua_State * L);
 static int l_confirm (lua_State * L);
 
@@ -55,6 +58,7 @@ static int l_confirm (lua_State * L);
 /* *INDENT-OFF* */
 static const struct luaL_Reg prompts_lib[] =
 {
+    { "_input", l_input },
     { "confirm", l_confirm },
     { "alert", l_alert },
     { NULL, NULL }
@@ -69,6 +73,54 @@ static const struct luaL_Reg prompts_global_lib[] =
 
 /* --------------------------------------------------------------------------------------------- */
 /*** file scope functions ************************************************************************/
+/* --------------------------------------------------------------------------------------------- */
+
+/**
+ * This function is exposed to Lua as "_input" and is wrapped by the
+ * higher-level "input".
+ *
+ * This function is background-safe.
+ */
+static int
+l_input (lua_State * L)
+{
+    const char *question, *def_text, *title, *history;
+    gboolean is_password = FALSE;
+
+    char *answer;
+
+    /* We default to "", not NULLs, as NULLs would crash MC (because strlen()
+     * is called on them later). */
+    /* *INDENT-OFF* */
+    question    = luaL_optstring(L, 1, "");
+    def_text    = luaMC_is_int_eq (L, 2, -1) ? INPUT_LAST_TEXT : luaL_optstring(L, 2, "");
+    title       = luaL_optstring(L, 3, "");
+    history     = luaL_optstring(L, 4, MC_HISTORY_LUA_DEFAULT);
+    is_password = lua_toboolean(L, 5);
+    /* *INDENT-ON* */
+
+#ifdef ENABLE_BACKGROUND
+    if (mc_global.we_are_background)
+    {
+        /* @FIXME: MC bug: INPUT_PASSWORD and INPUT_LAST_TEXT aren't background
+           safe because they can't be marshaled. They aren't real strings and
+           input_dialog_help() attempts to pass them to strlen(). */
+        is_password = FALSE;
+
+        if (def_text == INPUT_LAST_TEXT)
+            def_text = "";
+    }
+#endif
+
+    answer =
+        input_dialog (title, question, history, is_password ? INPUT_PASSWORD : def_text,
+                      INPUT_COMPLETE_NONE);
+    lua_pushstring (L, answer); /* NULL-safe. */
+    g_free (answer);
+
+    return 1;
+}
+
 /* --------------------------------------------------------------------------------------------- */
 
 /**
