mooffie/mc

240. classes ui.Canvas, ui.Custom; tty.style()
You can now create your own custom widgets, and draw on the screen.

src/lua/modules.h
@@ -15,6 +15,8 @@ int luaopen_regex (lua_State * L);
 int luaopen_timer (lua_State * L);
 int luaopen_tty (lua_State * L);
 int luaopen_ui (lua_State * L);
+int luaopen_ui_canvas (lua_State * L);
+int luaopen_ui_custom (lua_State * L);
 int luaopen_utils_bit32 (lua_State * L);
 int luaopen_utils_text (lua_State * L);
 

aborodin/mc-devel

src/lua/modules.h
@@ -24,7 +24,9 @@ int luaopen_prompts (lua_State *L);
 int luaopen_regex (lua_State *L);
 int luaopen_timer (lua_State *L);
 int luaopen_tty (lua_State *L);
-int luaopen_ui (lua_State * L);
+int luaopen_ui (lua_State *L);
+int luaopen_ui_canvas (lua_State *L);
+int luaopen_ui_custom (lua_State *L);
 int luaopen_utils_bit32 (lua_State *L);
 int luaopen_utils_text (lua_State *L);
 