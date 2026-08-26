mooffie/mc

345. [samples] module fs.fileops

src/lua/library/modules/samples/accessories/snapshots/core.lua
@@ -58,7 +58,7 @@ local function save_shots()
   local tmpf, tmp_path = fs.temporary_file()
   assert(tmpf:write(header, "return ", devel.pp(shots), "\n"))
   assert(tmpf:close())
-  assert(fs.rename(tmp_path, shots_path))
+  mc.mv(tmp_path, shots_path)
 end
 
 local function load_shots()

aborodin/mc-devel

src/lua/library/modules/samples/accessories/snapshots/core.lua
@@ -61,7 +61,7 @@ local header = [[
 ]]
 local function save_shots()
   local temp_path = fs.temporary_string_file(header, "return ", devel.pp(shots), "\n")
-  assert(fs.rename(temp_path, shots_path))
+  mc.mv(temp_path, shots_path)
 end
 
 local function load_shots()